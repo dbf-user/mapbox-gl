d3497e5a0ac22eac85dc91491ce91f7b6b54b191
Rishan Antonio <rishan.ok@gmail.com>
2023-08-29 11:52:55 +0530

    tripslayer update

--- app.jsx
+++ app.jsx
@@ -5,13 +5,11 @@ import { id1200 } from "./data/1200Id.js";
 import { id1500 } from "./data/1500Id.js";
 import { id900 } from "./data/900Id.js";
 import { idx } from "./data/bIds.js";
-import { MapboxLayer } from '@deck.gl/mapbox';
+import { MapboxLayer } from "@deck.gl/mapbox";
 import parks from "./data/parks.json";
-import {TripsLayer} from '@deck.gl/geo-layers';
+import { TripsLayer } from "@deck.gl/geo-layers";
 import park_names from "./data/park_names.json";
 import trips from "./data/tripp.json";
-
-
 
 import "./RadioPanel.css";
 import "./BuildingInfo.css";
@@ -27,7 +25,6 @@ import IsoApp from "./isoApp.jsx";
 import Co2App from "./co2.jsx";
 import pathways from "./data/trips.json";
 
-
 // Set your Mapbox token here
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZGlnaXRhbC1ibHVlLWZvYW0iLCJhIjoiY2w2b2h6aHE2MDd3NzNtcnI5ZjlieHkyZyJ9.lA1YnLC0rCHy9uUWQL0LDA";
@@ -36,10 +33,11 @@ let maxValue = 500;
 let minValue = 0;
 let title = "15 Parks";
 let buildingCount = 353;
+let animationId = null;
 let map;
 
 export function renderToDOM(container, data) {
-  map = new mapboxgl.Map({
+  const map = new mapboxgl.Map({
     style: "mapbox://styles/digital-blue-foam/clll4a01u01dc01plajw4bkhm",
     container,
     center: [-0.126323, 51.504758], // [-0.127997, 51.507969],
@@ -53,224 +51,235 @@ export function renderToDOM(container, data) {
     ],
   });
 
-  const [time, setTime] = useState(0);
-  const [animation] = useState({});
-
-  const animate = () => {
-    setTime(t => (t + 2) % 1800);
-    animation.id = window.requestAnimationFrame(animate);
-  };
-
-  useEffect(() => {
-    animation.id = window.requestAnimationFrame(animate);
-    return () => window.cancelAnimationFrame(animation.id);
-  }, [animation]);
-
-  const heatMarkersLayer = new MapboxLayer({
-    type: TripsLayer,
-    id: 'trips',
-      data: trips,
-      getPath: d => d.path,
-      getTimestamps: d => d.timestamps,
-      getColor: [253, 128, 93],
-      opacity: 0.3,
-      widthMinPixels: 4,
-      rounded: true,
-      trailLength: 180,
-      currentTime: time,
-
-      shadowEnabled: false
-  });
-
-
-  map.on("load", () => {
-    map.addLayer(heatMarkersLayer);
-    
-    map.loadImage("./data/park.png", (error, image) => {
-      if (error) throw error;
-
-      // Add the image to the map style.
-      map.addImage("cat", image);
-
-      // Add a data source containing one point feature.
-      map.addSource("point", {
-        type: "geojson",
-        data: park_names,
-      });
-
-      // Add a layer to use the image to represent the data.
-      map.addLayer({
-        id: "points",
-        type: "symbol",
-        source: "point", // reference the data source
-        layout: {
-          "icon-image": "cat", // reference the image
-          "icon-size": 0.75,
-        },
-      });
-    });
-
-    map.on("click", "points", (e) => {
-      // Copy coordinates array.
-      const coordinates = e.features[0].geometry.coordinates.slice();
-      const description = e.features[0].properties.name;
-
-      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-      }
-
-      new mapboxgl.Popup()
-        .setLngLat(coordinates)
-        .setHTML(description)
-        .addTo(map);
-    });
-
-    // Change the cursor to a pointer when the mouse is over the places layer.
-    map.on("mouseenter", "points", () => {
-      map.getCanvas().style.cursor = "pointer";
-    });
-
-    // Change it back to a pointer when it leaves.
-    map.on("mouseleave", "points", () => {
-      map.getCanvas().style.cursor = "";
-    });
-
-    map.addSource("buildings", {
-      type: "geojson",
-      data: parks,
-    });
-
-    // Add a GeoJSON layer with lines
-    map.addLayer({
-      id: "lines",
-      type: "fill",
-      source: "buildings",
-      paint: {
-        "fill-color": "#A7DD88",
-        "fill-emissive-strength": 0.2,
-        "fill-opacity": 0.8,
-      },
-    });
-
-    map.addLayer({
-      id: "add-3d-buildings",
-      source: "composite",
-      "source-layer": "building",
-      filter: ["==", "extrude", "true"],
-      type: "fill-extrusion",
-      minzoom: 13,
-      paint: {
-        "fill-extrusion-color": "#7182A6",
-        "fill-extrusion-ambient-occlusion-intensity": 0.8,
-        "fill-extrusion-height": [
-          "interpolate",
-          ["linear"],
-          ["zoom"],
-          15,
-          0,
-          15.05,
-          ["get", "height"],
-        ],
-        "fill-extrusion-base": [
-          "interpolate",
-          ["linear"],
-          ["zoom"],
-          15,
-          0,
-          15.05,
-          ["get", "min_height"],
-        ],
-        "fill-extrusion-opacity": 0.8,
-      },
-    });
-
-    // Add a GeoJSON layer with lines
-    map.addLayer({
-      id: "lines",
-      type: "fill",
-      source: "buildings",
-      paint: {
-        "fill-color": "#A7DD88",
-        "fill-emissive-strength": 0.5,
-        "fill-opacity": 0.8,
-      },
-    });
-    map.moveLayer("add-3d-buildings");
-
-    map.addSource("paths", {
-      type: "geojson",
-      data: pathways,
-      lineMetrics: true,
-    });
-
-    map.addLayer({
-      id: "tp-line-line",
-      type: "line",
-      source: "paths",
-      paint: {
-        "line-color": "rgba(0, 0, 0, 0)",
-        "line-emissive-strength": 2,
-        "line-width": 4,
-      },
-    });
-
-    map.flyTo({
-      center: [-0.128343, 51.511364],
-      essential: true, // this animation is considered essential with respect to prefers-reduced-motion
-      speed: 0.02,
-      zoom: 18,
-      curve: 1,
-      easing(t) {
-        return t;
-      },
-    });
-
-    let startTime;
-    const duration = 8000;
-  
-    const frame = (time) => {
-      if (!startTime) startTime = time;
-      const animationPhase = (time - startTime) / duration;
-
-      // Reduce the visible length of the line by using a line-gradient to cutoff the line
-      // animationPhase is a value between 0 and 1 that reprents the progress of the animation
-      map.setPaintProperty("tp-line-line", "line-gradient", [
-        "step",
-        ["line-progress"],
-        "#FD805D",
-        animationPhase,
-        "rgba(0, 0, 0, 0)",
-      ]);
-
-      if (animationPhase > 1) {
-        return;
-      }
-      window.requestAnimationFrame(frame);
-    };
-
-    window.requestAnimationFrame(frame);
-
-    // repeat
-    setInterval(() => {
-      startTime = undefined;
-      window.requestAnimationFrame(frame);
-    }, duration + 1500);
-  });
+  return map;
 }
 
 export const App = () => {
   const [selectedButton, setSelectedButton] = useState(null);
   const [showAnotherComponent, setShowAnotherComponent] = useState(false);
+  const [myTime, setTime] = useState(300);
+
+  const trailLength = 180;
 
   useEffect(() => {
-    renderToDOM(document.getElementById("map"));
+    const animate = () => {
+      if (animationId) {
+        window.cancelAnimationFrame(animationId);
+      }
+      animationId = window.requestAnimationFrame(animate);
+      setTime((t) => (t + 1) % 1800);
+    };
+
+    animate();
+    return () => window.cancelAnimationFrame(animationId);
+  }, []);
+
+  useEffect(() => {
+    if (map) {
+      if (map.getLayer("trips")) {
+        map.getLayer("trips").implementation.setProps({ currentTime: myTime });
+      }
+    }
+  }, [myTime]);
+
+  useEffect(() => {
+    map = renderToDOM(document.getElementById("map"));
+
+    const heatMarkersLayer = new MapboxLayer({
+      type: TripsLayer,
+      id: "trips",
+      data: trips,
+      getPath: (d) => d.path,
+      getTimestamps: (d) => d.timestamps,
+      getColor: [255, 0, 0],
+      opacity: 0.9,
+      widthMinPixels: 4,
+      jointRounded: true,
+      trailLength: 180,
+      currentTime: myTime,
+      shadowEnabled: false,
+    });
+
+    map.on("load", () => {
+      map.addLayer(heatMarkersLayer);
+
+      map.loadImage("./data/park.png", (error, image) => {
+        if (error) throw error;
+
+        // Add the image to the map style.
+        map.addImage("cat", image);
+
+        // Add a data source containing one point feature.
+        map.addSource("point", {
+          type: "geojson",
+          data: park_names,
+        });
+
+        // Add a layer to use the image to represent the data.
+        map.addLayer({
+          id: "points",
+          type: "symbol",
+          source: "point", // reference the data source
+          layout: {
+            "icon-image": "cat", // reference the image
+            "icon-size": 0.75,
+          },
+        });
+      });
+
+      map.on("click", "points", (e) => {
+        // Copy coordinates array.
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties.name;
+
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(description)
+          .addTo(map);
+      });
+
+      // Change the cursor to a pointer when the mouse is over the places layer.
+      map.on("mouseenter", "points", () => {
+        map.getCanvas().style.cursor = "pointer";
+      });
+
+      // Change it back to a pointer when it leaves.
+      map.on("mouseleave", "points", () => {
+        map.getCanvas().style.cursor = "";
+      });
+
+      map.addSource("buildings", {
+        type: "geojson",
+        data: parks,
+      });
+
+      // Add a GeoJSON layer with lines
+      map.addLayer({
+        id: "lines",
+        type: "fill",
+        source: "buildings",
+        paint: {
+          "fill-color": "#A7DD88",
+          "fill-emissive-strength": 0.2,
+          "fill-opacity": 0.8,
+        },
+      });
+
+      map.addLayer({
+        id: "add-3d-buildings",
+        source: "composite",
+        "source-layer": "building",
+        filter: ["==", "extrude", "true"],
+        type: "fill-extrusion",
+        minzoom: 13,
+        paint: {
+          "fill-extrusion-color": "#7182A6",
+          "fill-extrusion-ambient-occlusion-intensity": 0.8,
+          "fill-extrusion-height": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            15,
+            0,
+            15.05,
+            ["get", "height"],
+          ],
+          "fill-extrusion-base": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            15,
+            0,
+            15.05,
+            ["get", "min_height"],
+          ],
+          "fill-extrusion-opacity": 0.8,
+        },
+      });
+
+      // Add a GeoJSON layer with lines
+      map.addLayer({
+        id: "lines2",
+        type: "fill",
+        source: "buildings",
+        paint: {
+          "fill-color": "#A7DD88",
+          "fill-emissive-strength": 0.5,
+          "fill-opacity": 0.8,
+        },
+      });
+      map.moveLayer("add-3d-buildings");
+
+      map.addSource("paths", {
+        type: "geojson",
+        data: pathways,
+        lineMetrics: true,
+      });
+
+      map.addLayer({
+        id: "tp-line-line",
+        type: "line",
+        source: "paths",
+        paint: {
+          "line-color": "rgba(0, 0, 0, 0)",
+          "line-emissive-strength": 2,
+          "line-width": 4,
+        },
+      });
+
+      map.flyTo({
+        center: [-0.128343, 51.511364],
+        essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+        speed: 0.02,
+        zoom: 18,
+        curve: 1,
+        easing(t) {
+          return t;
+        },
+      });
+
+      let startTime;
+      const duration = 8000;
+
+      const frame = (time) => {
+        if (!startTime) startTime = time;
+        const animationPhase = (time - startTime) / duration;
+
+        // Reduce the visible length of the line by using a line-gradient to cutoff the line
+        // animationPhase is a value between 0 and 1 that reprents the progress of the animation
+        map.setPaintProperty("tp-line-line", "line-gradient", [
+          "step",
+          ["line-progress"],
+          "#FD805D",
+          animationPhase,
+          "rgba(0, 0, 0, 0)",
+        ]);
+
+        if (animationPhase > 1) {
+          return;
+        }
+        window.requestAnimationFrame(frame);
+      };
+
+      window.requestAnimationFrame(frame);
+
+      // repeat
+      setInterval(() => {
+        startTime = undefined;
+        window.requestAnimationFrame(frame);
+      }, duration + 1500);
+    });
   }, []);
 
   const handleButtonClick = (buttonId) => {
     if (selectedButton === buttonId) {
       setSelectedButton(null);
     } else {
-      setSelectedButton(buttonId); 
+      setSelectedButton(buttonId);
       setShowAnotherComponent(true);
     }
   };
@@ -289,11 +298,11 @@ export const App = () => {
   return (
     <>
       <div className={`app-container ${showAnotherComponent ? "hide" : ""}`}>
-      <div class="overlay">
-        <h1>AI-Powered Planning and Designis</h1>
+        <div class="overlay">
+          <h1>AI-Powered Planning and Designis</h1>
 
-        <h1>for Complex Urban Projects</h1>
-      </div>
+          <h1>for Complex Urban Projects</h1>
+        </div>
       </div>
       <div style={{ width: "100vw", height: "100vh" }}>
         <div
